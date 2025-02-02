b87034d359f83fdd9ba4dd68a5e54a4fb040342d
Karam Abbas <karamabbas03@gmail.com>
2025-02-02 17:01:21 +0500

    initialized the project

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Welcome to My First Next.js App</h1>
+      <p className="text-xl">Get started by editing src/app/page.js</p>
+    </main>
+  );
+}
